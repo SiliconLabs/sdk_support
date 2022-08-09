a7801b3fa493270b6832061ec9997eca3767ba67
jmartinez-silabs <67972863+jmartinez-silabs@users.noreply.github.com>
2022-08-09 09:40:29 -0400

    add config and autogen files for brd2601b, brd4186c and brd4187c (#28)
    * add config and autogen files for brd2601b, brd4186c and brd4187c
    
    * Restyled by whitespace
    
    * Restyled by clang-format
    
    * Fix comment header frame that the restyler screws up
    
    * Restyled by clang-format
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- matter/wifi/rs911x/hal/rsi_hal_mcu_rtc.c
+++ matter/wifi/rs911x/hal/rsi_hal_mcu_rtc.c
@@ -1,49 +1,53 @@
 /*******************************************************************************
-* @file  rsi_hal_mcu_rtc.c
-* @brief Hardware abstraction implementation for RTCC
-*******************************************************************************
-* # License
-* <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
-*******************************************************************************
-*
-* The licensor of this software is Silicon Laboratories Inc. Your use of this
-* software is governed by the terms of Silicon Labs Master Software License
-* Agreement (MSLA) available at
-* www.silabs.com/about-us/legal/master-software-license-agreement. This
-* software is distributed to you in Source Code format and is governed by the
-* sections of the MSLA applicable to Source Code.
-*
-******************************************************************************/
+ * @file  rsi_hal_mcu_rtc.c
+ * @brief Hardware abstraction implementation for RTCC
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
 
 /**
  * Includes
  */
-#include <time.h>
-#include <stdlib.h>
-#include "em_rtcc.h"
-#include "em_core.h"
 #include "em_cmu.h"
+#include "em_core.h"
+#include "em_rtcc.h"
 #include "sl_sleeptimer_config.h"
 #include "sl_status.h"
+#include <stdlib.h>
+#include <time.h>
 
-#define SLEEPTIMER_EVENT_OF   (0x01)
+#define SLEEPTIMER_EVENT_OF (0x01)
 #define SLEEPTIMER_EVENT_COMP (0x02)
-#define SLEEPTIMER_ENUM(name) \
-  typedef uint8_t name;       \
+#define SLEEPTIMER_ENUM(name)                                                  \
+  typedef uint8_t name;                                                        \
   enum name##_enum
-#define TIME_UNIX_EPOCH                     (1970u)
-#define TIME_NTP_EPOCH                      (1900u)
-#define TIME_ZIGBEE_EPOCH                   (2000u)
-#define TIME_NTP_UNIX_EPOCH_DIFF            (TIME_UNIX_EPOCH - TIME_NTP_EPOCH)
-#define TIME_ZIGBEE_UNIX_EPOCH_DIFF         (TIME_ZIGBEE_EPOCH - TIME_UNIX_EPOCH)
-#define TIME_DAY_COUNT_NTP_TO_UNIX_EPOCH    (TIME_NTP_UNIX_EPOCH_DIFF * 365u + 17u)   ///< 70 years and 17 leap days
-#define TIME_DAY_COUNT_ZIGBEE_TO_UNIX_EPOCH (TIME_ZIGBEE_UNIX_EPOCH_DIFF * 365u + 7u) ///< 30 years and 7 leap days
-#define TIME_SEC_PER_DAY                    (60u * 60u * 24u)
-#define TIME_NTP_EPOCH_OFFSET_SEC           (TIME_DAY_COUNT_NTP_TO_UNIX_EPOCH * TIME_SEC_PER_DAY)
-#define TIME_ZIGBEE_EPOCH_OFFSET_SEC        (TIME_DAY_COUNT_ZIGBEE_TO_UNIX_EPOCH * TIME_SEC_PER_DAY)
-#define TIME_DAY_PER_YEAR                   (365u)
-#define TIME_SEC_PER_YEAR                   (TIME_SEC_PER_DAY * TIME_DAY_PER_YEAR)
-#define TIME_UNIX_TIMESTAMP_MAX             (0x7FFFFFFF)
+#define TIME_UNIX_EPOCH (1970u)
+#define TIME_NTP_EPOCH (1900u)
+#define TIME_ZIGBEE_EPOCH (2000u)
+#define TIME_NTP_UNIX_EPOCH_DIFF (TIME_UNIX_EPOCH - TIME_NTP_EPOCH)
+#define TIME_ZIGBEE_UNIX_EPOCH_DIFF (TIME_ZIGBEE_EPOCH - TIME_UNIX_EPOCH)
+#define TIME_DAY_COUNT_NTP_TO_UNIX_EPOCH                                       \
+  (TIME_NTP_UNIX_EPOCH_DIFF * 365u + 17u) ///< 70 years and 17 leap days
+#define TIME_DAY_COUNT_ZIGBEE_TO_UNIX_EPOCH                                    \
+  (TIME_ZIGBEE_UNIX_EPOCH_DIFF * 365u + 7u) ///< 30 years and 7 leap days
+#define TIME_SEC_PER_DAY (60u * 60u * 24u)
+#define TIME_NTP_EPOCH_OFFSET_SEC                                              \
+  (TIME_DAY_COUNT_NTP_TO_UNIX_EPOCH * TIME_SEC_PER_DAY)
+#define TIME_ZIGBEE_EPOCH_OFFSET_SEC                                           \
+  (TIME_DAY_COUNT_ZIGBEE_TO_UNIX_EPOCH * TIME_SEC_PER_DAY)
+#define TIME_DAY_PER_YEAR (365u)
+#define TIME_SEC_PER_YEAR (TIME_SEC_PER_DAY * TIME_DAY_PER_YEAR)
+#define TIME_UNIX_TIMESTAMP_MAX (0x7FFFFFFF)
 
 /// Time zone offset from UTC(second).
 typedef int32_t sl_sleeptimer_time_zone_offset_t;
@@ -51,10 +55,13 @@ typedef int32_t sl_sleeptimer_time_zone_offset_t;
 typedef uint32_t sl_sleeptimer_timestamp_t;
 /// @brief Time Format.
 SLEEPTIMER_ENUM(sl_sleeptimer_time_format_t){
-  TIME_FORMAT_UNIX = 0, ///< Number of seconds since January 1, 1970, 00:00. Type is signed, so represented on 31 bit.
-  TIME_FORMAT_NTP  = 1, ///< Number of seconds since January 1, 1900, 00:00. Type is unsigned, so represented on 32 bit.
-  TIME_FORMAT_ZIGBEE_CLUSTER =
-    2, ///< Number of seconds since January 1, 2000, 00:00. Type is unsigned, so represented on 32 bit.
+    TIME_FORMAT_UNIX = 0, ///< Number of seconds since January 1, 1970, 00:00.
+                          ///< Type is signed, so represented on 31 bit.
+    TIME_FORMAT_NTP = 1,  ///< Number of seconds since January 1, 1900, 00:00.
+                          ///< Type is unsigned, so represented on 32 bit.
+    TIME_FORMAT_ZIGBEE_CLUSTER =
+        2, ///< Number of seconds since January 1, 2000, 00:00. Type is
+           ///< unsigned, so represented on 32 bit.
 };
 
 // Initialization flag.
@@ -80,23 +87,25 @@ static uint32_t overflow_tick_rest = 0;
  ******************************************************************************/
 static bool is_valid_time(sl_sleeptimer_timestamp_t time,
                           sl_sleeptimer_time_format_t format,
-                          sl_sleeptimer_time_zone_offset_t time_zone)
-{
+                          sl_sleeptimer_time_zone_offset_t time_zone) {
   bool valid_time = false;
 
   // Check for overflow.
-  if ((time_zone < 0 && time > (uint32_t)abs(time_zone)) || (time_zone >= 0 && (time <= UINT32_MAX - time_zone))) {
+  if ((time_zone < 0 && time > (uint32_t)abs(time_zone)) ||
+      (time_zone >= 0 && (time <= UINT32_MAX - time_zone))) {
     valid_time = true;
   }
   if (format == TIME_FORMAT_UNIX) {
-    if (time > TIME_UNIX_TIMESTAMP_MAX) { // Check if Unix time stamp is an unsigned 31 bits.
+    if (time > TIME_UNIX_TIMESTAMP_MAX) { // Check if Unix time stamp is an
+                                          // unsigned 31 bits.
       valid_time = false;
     }
   } else {
     if ((format == TIME_FORMAT_NTP) && (time >= TIME_NTP_EPOCH_OFFSET_SEC)) {
       valid_time &= true;
-    } else if ((format == TIME_FORMAT_ZIGBEE_CLUSTER)
-               && (time <= TIME_UNIX_TIMESTAMP_MAX - TIME_ZIGBEE_EPOCH_OFFSET_SEC)) {
+    } else if ((format == TIME_FORMAT_ZIGBEE_CLUSTER) &&
+               (time <=
+                TIME_UNIX_TIMESTAMP_MAX - TIME_ZIGBEE_EPOCH_OFFSET_SEC)) {
       valid_time &= true;
     } else {
       valid_time = false;
@@ -108,27 +117,28 @@ static bool is_valid_time(sl_sleeptimer_timestamp_t time,
 /*******************************************************************************
  * Gets RTCC timer frequency.
  ******************************************************************************/
-uint32_t rsi_rtc_get_hal_timer_frequency(void)
-{
-  return (CMU_ClockFreqGet(cmuClock_RTCC) >> (CMU_PrescToLog2(SL_SLEEPTIMER_FREQ_DIVIDER - 1)));
+uint32_t rsi_rtc_get_hal_timer_frequency(void) {
+  return (CMU_ClockFreqGet(cmuClock_RTCC) >>
+          (CMU_PrescToLog2(SL_SLEEPTIMER_FREQ_DIVIDER - 1)));
 }
 
 /******************************************************************************
  * Initializes RTCC sleep timer.
  *****************************************************************************/
-void rsi_rtc_init_timer(void)
-{
-  RTCC_Init_TypeDef rtcc_init   = RTCC_INIT_DEFAULT;
+void rsi_rtc_init_timer(void) {
+  RTCC_Init_TypeDef rtcc_init = RTCC_INIT_DEFAULT;
   RTCC_CCChConf_TypeDef channel = RTCC_CH_INIT_COMPARE_DEFAULT;
 
   CMU_ClockEnable(cmuClock_RTCC, true);
 
   rtcc_init.enable = false;
-  rtcc_init.presc  = (RTCC_CntPresc_TypeDef)(CMU_PrescToLog2(SL_SLEEPTIMER_FREQ_DIVIDER - 1));
+  rtcc_init.presc =
+      (RTCC_CntPresc_TypeDef)(CMU_PrescToLog2(SL_SLEEPTIMER_FREQ_DIVIDER - 1));
 
   RTCC_Init(&rtcc_init);
 
-  // Compare channel starts disabled and is enabled only when compare match interrupt is enabled.
+  // Compare channel starts disabled and is enabled only when compare match
+  // interrupt is enabled.
   channel.chMode = rtccCapComChModeOff;
   RTCC_ChannelInit(1u, &channel);
 
@@ -145,8 +155,7 @@ void rsi_rtc_init_timer(void)
 /******************************************************************************
  * Enables RTCC interrupts.
  *****************************************************************************/
-void rsi_rtc_enable_int(uint8_t local_flag)
-{
+void rsi_rtc_enable_int(uint8_t local_flag) {
   uint32_t rtcc_ien = 0u;
 
   if (local_flag & SLEEPTIMER_EVENT_OF) {
@@ -160,24 +169,17 @@ void rsi_rtc_enable_int(uint8_t local_flag)
   RTCC_IntEnable(rtcc_ien);
 }
 
-/***************************************************************************/ /**
+/*******************************************************************************
  * Get timer frequency.
  ******************************************************************************/
-uint32_t rsi_rtc_get_timer_frequency(void)
-{
-  return timer_frequency;
-}
+uint32_t rsi_rtc_get_timer_frequency(void) { return timer_frequency; }
 
 /******************************************************************************
  * Gets RTCC counter value.
  *****************************************************************************/
-uint32_t rsi_rtc_get_counter(void)
-{
-  return RTCC_CounterGet();
-}
+uint32_t rsi_rtc_get_counter(void) { return RTCC_CounterGet(); }
 
-sl_status_t rsi_rtc_init(void)
-{
+sl_status_t rsi_rtc_init(void) {
   CORE_DECLARE_IRQ_STATE;
   CORE_ENTER_ATOMIC();
 
@@ -202,17 +204,16 @@ sl_status_t rsi_rtc_init(void)
 }
 
 #if SL_SLEEPTIMER_WALLCLOCK_CONFIG
-/***************************************************************************/ /**
+/*******************************************************************************
  * Retrieves current time.
  ******************************************************************************/
-uint32_t rsi_rtc_get_time(void)
-{
-  uint32_t cnt  = 0u;
+uint32_t rsi_rtc_get_time(void) {
+  uint32_t cnt = 0u;
   uint32_t freq = 0u;
   sl_sleeptimer_timestamp_t time;
   CORE_DECLARE_IRQ_STATE;
 
-  cnt  = rsi_rtc_get_counter();
+  cnt = rsi_rtc_get_counter();
   freq = rsi_rtc_get_timer_frequency();
 
   CORE_ENTER_ATOMIC();
@@ -225,14 +226,13 @@ uint32_t rsi_rtc_get_time(void)
   return time;
 }
 
-/***************************************************************************/ /**
+/*******************************************************************************
  * Sets current time.
  ******************************************************************************/
-sl_status_t rsi_rtc_settime(sl_sleeptimer_timestamp_t time)
-{
-  uint32_t freq        = 0u;
+sl_status_t rsi_rtc_settime(sl_sleeptimer_timestamp_t time) {
+  uint32_t freq = 0u;
   uint32_t counter_sec = 0u;
-  uint32_t cnt         = 0;
+  uint32_t cnt = 0;
   CORE_DECLARE_IRQ_STATE;
 
   if (!is_valid_time(time, TIME_FORMAT_UNIX, 0u)) {
@@ -240,12 +240,12 @@ sl_status_t rsi_rtc_settime(sl_sleeptimer_timestamp_t time)
   }
 
   freq = rsi_rtc_get_timer_frequency();
-  cnt  = rsi_rtc_get_counter();
+  cnt = rsi_rtc_get_counter();
 
   CORE_ENTER_ATOMIC();
-  second_count       = time;
+  second_count = time;
   overflow_tick_rest = 0;
-  counter_sec        = cnt / freq;
+  counter_sec = cnt / freq;
 
   if (second_count >= counter_sec) {
     second_count -= counter_sec;
@@ -259,13 +259,12 @@ sl_status_t rsi_rtc_settime(sl_sleeptimer_timestamp_t time)
   return SL_STATUS_OK;
 }
 
-/***************************************************************************/ /**
+/*******************************************************************************
  * Init Sleeptimer and Set current time.
  ******************************************************************************/
-int32_t rsi_rtc_set_time(uint32_t time)
-{
+int32_t rsi_rtc_set_time(uint32_t time) {
   sl_status_t ret_val = SL_STATUS_OK;
-  ret_val             = rsi_rtc_init();
+  ret_val = rsi_rtc_init();
   if (ret_val != SL_STATUS_OK) {
     return -1;
   } else {
