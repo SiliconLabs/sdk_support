c9d65e0111fb7708d41f86bc999bb22de43b5915
bhmanda-silabs <107180296+bhmanda-silabs@users.noreply.github.com>
2022-08-11 20:37:39 +0530

    Added board configs for MG24 4186C and 4187C (#31)
    * Added board configs for MG24 4186C and 4187C
    
    * Restyled by clang-format
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    Co-authored-by: chirag-silabs <100861685+chirag-silabs@users.noreply.github.com>

--- matter/wifi/rs911x/hal/rsi_board_configuration.h
+++ matter/wifi/rs911x/hal/rsi_board_configuration.h
@@ -11,21 +11,19 @@ typedef struct {
 //#define CONCAT_EXPAND(a, b)      CONCAT(a,b)
 //#define CONCAT3_EXPAND(a, b, c)  CONCAT3(a,b,c)
 
-#define PIN(port_id, pin_id)                 \
-  (rsi_pin_t)                                \
-  {                                          \
-    .port = gpioPort##port_id, .pin = pin_id \
-  }
+#define PIN(port_id, pin_id)                                                   \
+  (rsi_pin_t) { .port = gpioPort##port_id, .pin = pin_id }
 
 #if defined(EFR32MG21_BRD4180A) || defined(BRD4180A)
 #include "brd4180a.h"
-#elif defined(EFR32MG12_BRD4161A) || defined(BRD4161A) || defined(EFR32MG12_BRD4163A) || defined(BRD4163A) \
-  || defined(EFR32MG12_BRD4164A) || defined(BRD4164A)
+#elif defined(EFR32MG12_BRD4161A) || defined(BRD4161A) ||                      \
+    defined(EFR32MG12_BRD4163A) || defined(BRD4163A) ||                        \
+    defined(EFR32MG12_BRD4164A) || defined(BRD4164A)
 // BRD4161-63-64 are pin to pin compatible for SPI
 #include "brd4161a.h"
-#elif  defined(EFR32MG12_BRD4186A) || defined(BRD4186A)
+#elif defined(EFR32MG24_BRD4186C) || defined(BRD4186C)
 #include "brd4186c.h"
-#elif defined(EFR32MG24_BRD4187A) || defined(BRD4187A)
+#elif defined(EFR32MG24_BRD4187C) || defined(BRD4187C)
 #include "brd4187c.h"
 #else
 #error "Need SPI Pins"
