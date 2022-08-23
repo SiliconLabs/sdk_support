78d7e9f744154f48e4e9441d5cac8e5456258add
Restyled.io <commits@restyled.io>
2022-08-23 13:08:41 +0000

    Restyled by clang-format

--- matter/efr32/efr32mg24/BRD2703A/autogen/sl_event_handler.c
+++ matter/efr32/efr32mg24/BRD2703A/autogen/sl_event_handler.c
@@ -1,38 +1,37 @@
+#include "btl_interface.h"
 #include "em_chip.h"
-#include "sl_device_init_nvic.h"
+#include "gpiointerrupt.h"
+#include "nvm3_default.h"
+#include "pa_conversions_efr32.h"
+#include "sl_board_control.h"
 #include "sl_board_init.h"
+#include "sl_bt_rtos_adaptation.h"
+#include "sl_device_init_clocks.h"
 #include "sl_device_init_dcdc.h"
-#include "sl_hfxo_manager.h"
+#include "sl_device_init_dpll.h"
+#include "sl_device_init_emu.h"
 #include "sl_device_init_hfxo.h"
 #include "sl_device_init_lfrco.h"
 #include "sl_device_init_lfxo.h"
-#include "sl_device_init_clocks.h"
-#include "sl_device_init_emu.h"
-#include "pa_conversions_efr32.h"
-#include "sl_rail_util_pti.h"
-#include "btl_interface.h"
-#include "sl_board_control.h"
-#include "sl_bt_rtos_adaptation.h"
-#include "sl_sleeptimer.h"
-#include "gpiointerrupt.h"
+#include "sl_device_init_nvic.h"
+#include "sl_hfxo_manager.h"
 #include "sl_mbedtls.h"
 #include "sl_mpu.h"
-#include "nvm3_default.h"
+#include "sl_rail_util_pti.h"
 #include "sl_simple_button_instances.h"
 #include "sl_simple_led_instances.h"
-#include "sl_device_init_dpll.h"
+#include "sl_sleeptimer.h"
 
 #if defined(CONFIG_ENABLE_UART)
-#include "sl_uartdrv_instances.h"
-#include "psa/crypto.h"
-#include "sli_protocol_crypto.h"
 #include "cmsis_os2.h"
+#include "psa/crypto.h"
 #include "sl_bluetooth.h"
 #include "sl_power_manager.h"
+#include "sl_uartdrv_instances.h"
+#include "sli_protocol_crypto.h"
 #endif
 
-void sl_platform_init(void)
-{
+void sl_platform_init(void) {
   CHIP_Init();
   sl_device_init_nvic();
   sl_board_preinit();
@@ -53,13 +52,9 @@ void sl_platform_init(void)
 #endif
 }
 
-void sl_kernel_start(void)
-{
-  osKernelStart();
-}
+void sl_kernel_start(void) { osKernelStart(); }
 
-void sl_driver_init(void)
-{
+void sl_driver_init(void) {
   GPIOINT_Init();
   sl_simple_button_init_instances();
   sl_simple_led_init_instances();
@@ -68,8 +63,7 @@ void sl_driver_init(void)
 #endif // CONFIG_ENABLE_UART
 }
 
-void sl_service_init(void)
-{
+void sl_service_init(void) {
   sl_board_configure_vcom();
   sl_sleeptimer_init();
   sl_hfxo_manager_init();
@@ -79,28 +73,17 @@ void sl_service_init(void)
   sli_aes_seed_mask();
 }
 
-void sl_stack_init(void)
-{
+void sl_stack_init(void) {
   sl_rail_util_pa_init();
   sl_rail_util_pti_init();
 }
 
-void sl_internal_app_init(void)
-{
-}
+void sl_internal_app_init(void) {}
 
-void sl_platform_process_action(void)
-{
-}
+void sl_platform_process_action(void) {}
 
-void sl_service_process_action(void)
-{
-}
+void sl_service_process_action(void) {}
 
-void sl_stack_process_action(void)
-{
-}
+void sl_stack_process_action(void) {}
 
-void sl_internal_app_process_action(void)
-{
-}
+void sl_internal_app_process_action(void) {}
