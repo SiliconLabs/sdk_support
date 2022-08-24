f8c2e1c3dc9f4315412cb64cceaf48a57654098a
Restyled.io <commits@restyled.io>
2022-08-24 11:24:38 +0000

    Restyled by clang-format

--- matter/wifi/brd4187c.h
+++ matter/wifi/brd4187c.h
@@ -1,46 +1,47 @@
 #ifndef _BRD4187C_H_
 #define _BRD4187C_H_
 #ifndef LOGGING_STATS
-#define WAKE_INDICATOR_PIN  PIN(D, 3)
+#define WAKE_INDICATOR_PIN PIN(D, 3)
 #endif
 
 #ifdef LOGGING_STATS
-#define LOGGING_WAKE_INDICATOR_PIN  PIN(D, 3)
-#define LOGGING_STATS_PORT    gpioPortD
-#define LOGGING_STATS_PIN     03
+#define LOGGING_WAKE_INDICATOR_PIN PIN(D, 3)
+#define LOGGING_STATS_PORT gpioPortD
+#define LOGGING_STATS_PIN 03
 #endif
 
 #ifdef RS911X_WIFI
 // SPI ports and pins
-#define EUS1MOSI_PORT   gpioPortC
-#define EUS1MOSI_PIN    1
-#define EUS1MISO_PORT   gpioPortC
-#define EUS1MISO_PIN    2
-#define EUS1SCLK_PORT   gpioPortC
-#define EUS1SCLK_PIN    3
-#define EUS1CS_PORT     gpioPortC
-#define EUS1CS_PIN      0
-
+#define EUS1MOSI_PORT gpioPortC
+#define EUS1MOSI_PIN 1
+#define EUS1MISO_PORT gpioPortC
+#define EUS1MISO_PIN 2
+#define EUS1SCLK_PORT gpioPortC
+#define EUS1SCLK_PIN 3
+#define EUS1CS_PORT gpioPortC
+#define EUS1CS_PIN 0
 
 #define MY_USART EUSART1
 #define MY_USART_CLOCK cmuClock_EUSART1
-#define MY_USART_TX_SIGNAL dmadrvPeripheralSignal_EUSART1_TXBL //dmadrvPeripheralSignal_USART0_TXBL
-#define MY_USART_RX_SIGNAL dmadrvPeripheralSignal_EUSART1_RXDATAV //dmadrvPeripheralSignal_USART0_RXDATAV
+#define MY_USART_TX_SIGNAL                                                     \
+  dmadrvPeripheralSignal_EUSART1_TXBL // dmadrvPeripheralSignal_USART0_TXBL
+#define MY_USART_RX_SIGNAL                                                     \
+  dmadrvPeripheralSignal_EUSART1_RXDATAV // dmadrvPeripheralSignal_USART0_RXDATAV
 
-#define WFX_RESET_PIN           PIN(A, 6)
-#define WFX_INTERRUPT_PIN       PIN(A, 7)
-#define WFX_SLEEP_CONFIRM_PIN   PIN(A, 5) /* Exp hdr 7 */
+#define WFX_RESET_PIN PIN(A, 6)
+#define WFX_INTERRUPT_PIN PIN(A, 7)
+#define WFX_SLEEP_CONFIRM_PIN PIN(A, 5) /* Exp hdr 7 */
 #define SL_WFX_HOST_PINOUT_SPI_IRQ 5
 
 #else /* WF200 */
 
-#define PIN_OUT_SET	    	1
-#define PIN_OUT_CLEAR		0
+#define PIN_OUT_SET 1
+#define PIN_OUT_CLEAR 0
 
-#define MY_USART            USART0
-#define MY_USART_CLOCK      cmuClock_USART0
-#define MY_USART_TX_SIGNAL  dmadrvPeripheralSignal_USART0_TXBL
-#define MY_USART_RX_SIGNAL  dmadrvPeripheralSignal_USART0_RXDATAV
+#define MY_USART USART0
+#define MY_USART_CLOCK cmuClock_USART0
+#define MY_USART_TX_SIGNAL dmadrvPeripheralSignal_USART0_TXBL
+#define MY_USART_RX_SIGNAL dmadrvPeripheralSignal_USART0_RXDATAV
 
 #define SL_WFX_HOST_PINOUT_RESET_PORT gpioPortA
 #define SL_WFX_HOST_PINOUT_RESET_PIN 5
@@ -68,7 +69,7 @@
 #endif /* WF200/9116 */
 
 #ifdef RS911X_WIFI
-#else /* WF200 */
+#else  /* WF200 */
 #endif /* WF200/9116 */
 
 #endif /* _BRD4187C_H_ */
