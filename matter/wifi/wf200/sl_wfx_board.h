736c828ff89708e0f74f040dabe0483cde450fe2
Chirag Bansal <chirag.bansal@silabs.com>
2022-09-02 13:21:36 +0530

    Adding brd4162a support for wf200

--- matter/wifi/wf200/sl_wfx_board.h
+++ matter/wifi/wf200/sl_wfx_board.h
@@ -5,7 +5,7 @@
  */
 #if defined(EFR32MG21_BRD4180A) || defined(BRD4180A)
 #include "brd4180a.h"
-#elif defined(EFR32MG12_BRD4161A) || defined(BRD4161A) || defined(EFR32MG12_BRD4163A) || defined(BRD4163A) \
+#elif defined(EFR32MG12_BRD4161A) || defined(BRD4161A) || defined(EFR32MG12_BRD4162A) || defined(BRD4162A) || defined(EFR32MG12_BRD4163A) || defined(BRD4163A) \
   || defined(EFR32MG12_BRD4164A) || defined(BRD4164A)
 #include "brd4161a.h"
 #elif defined(EFR32MG24_BRD4187A) || defined(BRD4187A)
