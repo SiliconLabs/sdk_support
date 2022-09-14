44cb662a4a8e69db593dcbd94d495a4cc92557cf
Sharad Patil <sharad.patil@silabs.com>
2022-09-14 23:44:23 +0530

    Review comment solved

--- matter/wifi/wf200/sl_wfx_board.h
+++ matter/wifi/wf200/sl_wfx_board.h
@@ -8,11 +8,13 @@
     defined(EFR32MG12_BRD4163A) || defined(BRD4163A) ||           \
     defined(EFR32MG12_BRD4164A) || defined(BRD4164A)
 #include "brd4161a.h"
-#elif defined(EFR32MG24_BRD4186C) || defined(BRD4186C)
+#elif defined(EFR32MG24_BRD4186C) || defined(BRD4186C)            \
+      defined(EFR32MG24_BRD4186A) || defined(BRD4186A)
 #include "brd4186c.h"
-#elif defined(EFR32MG24_BRD4187C) || defined(BRD4187C)
+#elif defined(EFR32MG24_BRD4187C) || defined(BRD4187C)            \
+      defined(EFR32MG24_BRD4187A) || defined(BRD4187A)
 #include "brd4187c.h"
 #else
 #error "Need SPI Pins"
-#endif /* EFR32MG12ZZ_BRD4161A */
+#endif /* EFR32MG12_BRD4161A */
 #endif /* _SL_WFX_BOARD_H_ */
