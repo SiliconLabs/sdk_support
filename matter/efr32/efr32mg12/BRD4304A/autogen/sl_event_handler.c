d1f7c2333d3ed7134f24347577d78a909a7ed1a0
Mathieu Kardous <mathieu.kardous@silabs.com>
2022-09-22 12:50:04 -0400

    Align Bluetooth generation for BRD4304A

--- matter/efr32/efr32mg12/BRD4304A/autogen/sl_event_handler.c
+++ matter/efr32/efr32mg12/BRD4304A/autogen/sl_event_handler.c
@@ -14,12 +14,14 @@
 #include "sl_device_init_nvic.h"
 #include "sl_hfxo_manager.h"
 #include "sl_rail_util_pti.h"
+#include "sl_bt_rtos_adaptation.h"
 #include "sl_simple_button_instances.h"
 #include "sl_simple_led_instances.h"
 #include "sl_sleeptimer.h"
 #if defined(CONFIG_ENABLE_UART)
 #include "sl_uartdrv_instances.h"
 #endif // CONFIG_ENABLE_UART
+#include "sl_bluetooth.h"
 #if defined(SL_CATALOG_POWER_MANAGER_PRESENT)
 #include "sl_power_manager.h"
 #endif
@@ -63,8 +65,10 @@ void sl_service_init(void)
 
 void sl_stack_init(void)
 {
-    sl_rail_util_pa_init();
-    sl_rail_util_pti_init();
+  sl_fem_util_init();
+  sl_rail_util_pa_init();
+  sl_rail_util_pti_init();
+  sl_bt_rtos_init();
 }
 
 void sl_internal_app_init(void) {}
