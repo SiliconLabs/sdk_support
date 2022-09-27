9f985b547501dae4d86ee39506f7314889dd8213
Mathieu Kardous <mathieu.kardous@silabs.com>
2022-09-27 17:44:29 -0400

    Fix BRD4163A generation

--- matter/efr32/efr32mg12/BRD4163A/config/app_assert_config.h
+++ matter/efr32/efr32mg12/BRD4163A/config/app_assert_config.h
@@ -1,9 +1,9 @@
 /***************************************************************************//**
  * @file
- * @brief DEVICE_INIT_HFXO Config
+ * @brief Application assert configuration
  *******************************************************************************
  * # License
- * <b>Copyright 2022 Silicon Laboratories Inc. www.silabs.com</b>
+ * <b>Copyright 2021 Silicon Laboratories Inc. www.silabs.com</b>
  *******************************************************************************
  *
  * SPDX-License-Identifier: Zlib
@@ -28,41 +28,27 @@
  *
  ******************************************************************************/
 
-#ifndef SL_DEVICE_INIT_HFXO_CONFIG_H
-#define SL_DEVICE_INIT_HFXO_CONFIG_H
+#ifndef APP_ASSERT_CONFIG_H
+#define APP_ASSERT_CONFIG_H
 
 // <<< Use Configuration Wizard in Context Menu >>>
 
-// <o SL_DEVICE_INIT_HFXO_MODE> Mode
-// <i>
-// <cmuOscMode_Crystal=> Crystal oscillator
-// <cmuOscMode_External=> External digital clock
-// <i> Default: cmuOscMode_Crystal
-#define SL_DEVICE_INIT_HFXO_MODE           cmuOscMode_Crystal
+// <e APP_ASSERT_ENABLE> Assert component
+// <i> Enables Assert.
+#define APP_ASSERT_ENABLE      1
 
-// <o SL_DEVICE_INIT_HFXO_FREQ> Frequency <38000000-40000000>
-// <i> Default: 38400000
-#define SL_DEVICE_INIT_HFXO_FREQ           38400000
+// <e APP_ASSERT_LOG_ENABLE> Logging
+// <i> Enables logging for assert.
+#define APP_ASSERT_LOG_ENABLE      1
 
-// <o SL_DEVICE_INIT_HFXO_CTUNE> CTUNE <0-511>
-// <i> Default: 360
-#define SL_DEVICE_INIT_HFXO_CTUNE          331
+// <q APP_ASSERT_TRACE_ENABLE> Enable trace
+// <i> Enables trace for assert.
+#define APP_ASSERT_TRACE_ENABLE      1
 
-// <h> Advanced Configurations
-// <o SL_DEVICE_INIT_HFXO_AUTOSTART> Auto-start HFXO. This feature is incompatible with Power Manager and can only be enabled in applications that do not use Power Manager or a radio protocol stack. - DEPRECATED
-// <true=> True
-// <false=> False
-// <i> Default: false
-#define SL_DEVICE_INIT_HFXO_AUTOSTART      false
+// </e>
 
-// <o SL_DEVICE_INIT_HFXO_AUTOSELECT> Auto-select HFXO. This feature is incompatible with Power Manager and can only be enabled in applications that do not use Power Manager or a radio protocol stack. - DEPRECATED
-// <true=> True
-// <false=> False
-// <i> Default: false
-#define SL_DEVICE_INIT_HFXO_AUTOSELECT      false
-
-// </h>
+// </e>
 
 // <<< end of configuration section >>>
 
-#endif // SL_DEVICE_INIT_HFXO_CONFIG_H
+#endif // APP_ASSERT_CONFIG_H
