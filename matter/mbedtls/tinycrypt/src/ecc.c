8d3c6351d4508095d1e0f0326e8092ccb02fb733
Restyled.io <commits@restyled.io>
2022-11-26 22:55:28 +0000

    Restyled by clang-format

--- matter/mbedtls/tinycrypt/src/ecc.c
+++ matter/mbedtls/tinycrypt/src/ecc.c
@@ -777,8 +777,8 @@ static __asm void muladd(uECC_word_t a, uECC_word_t b, uECC_word_t r[3]) {
       r1 // r5 := b.lo
           LSRS r1,
       r1,
-# 16 // r1 := b.hi
-      // Multiply halfword pairs
+# 16 // r1 := b.hi                                                             \
+    // Multiply halfword pairs
       MOVS r0, r3 MULS r0, r5,
       r0 // r0 := a.lo * b.lo
           MULS r3,
