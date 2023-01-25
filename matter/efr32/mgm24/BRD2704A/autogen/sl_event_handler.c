881c8e46cd2745f800c77ca40aa1f0fa2c1accdf
Jean-Francois Penven <67962328+jepenven-silabs@users.noreply.github.com>
2023-01-25 13:29:30 -0500

    Add BRD2704A & BRD4318A support (#102)

---
+++ matter/efr32/mgm24/BRD2704A/autogen/sl_event_handler.c
@@ -0,0 +1,92 @@
+#include "sl_event_handler.h"
+
+#include "em_chip.h"
+#include "sl_device_init_nvic.h"
+#include "sl_board_init.h"
+#include "sl_device_init_dcdc.h"
+#include "sl_hfxo_manager.h"
+#include "sl_device_init_hfxo.h"
+#include "sl_device_init_lfrco.h"
+#include "sl_device_init_lfxo.h"
+#include "sl_device_init_dpll.h"
+#include "sl_device_init_clocks.h"
+#include "sl_device_init_emu.h"
+#include "sl_fem_util.h"
+#include "pa_conversions_efr32.h"
+#include "sl_rail_util_pti.h"
+#include "sl_rail_util_rssi.h"
+#include "btl_interface.h"
+#include "sl_board_control.h"
+#include "sl_bt_rtos_adaptation.h"
+#include "sl_sleeptimer.h"
+#include "sl_debug_swo.h"
+#include "gpiointerrupt.h"
+#include "sl_mbedtls.h"
+#include "sl_mpu.h"
+#include "nvm3_default.h"
+#include "sl_uartdrv_instances.h"
+#include "psa/crypto.h"
+#include "sli_protocol_crypto.h"
+#include "cmsis_os2.h"
+#include "sl_bluetooth.h"
+#include "sl_power_manager.h"
+#include "sl_cos.h"
+#include "sl_simple_led_instances.h"
+
+void sl_platform_init(void)
+{
+  CHIP_Init();
+  sl_device_init_nvic();
+  sl_board_preinit();
+  sl_device_init_dcdc();
+  sl_hfxo_manager_init_hardware();
+  sl_device_init_hfxo();
+  sl_device_init_lfrco();
+  sl_device_init_dpll();
+  sl_device_init_clocks();
+  sl_device_init_emu();
+  sl_board_init();
+  bootloader_init();
+  nvm3_initDefault();
+  osKernelInitialize();
+  sl_power_manager_init();
+}
+
+void sl_kernel_start(void)
+{
+  sli_bt_rtos_adaptation_kernel_start();
+  osKernelStart();
+}
+
+void sl_driver_init(void)
+{
+  sl_debug_swo_init();
+  GPIOINT_Init();
+  sl_simple_led_init_instances();
+  sl_uartdrv_init_instances();
+  sl_cos_send_config();
+}
+
+void sl_service_init(void)
+{
+  sl_board_configure_vcom();
+  sl_sleeptimer_init();
+  sl_hfxo_manager_init();
+  sl_mbedtls_init();
+  sl_mpu_disable_execute_from_ram();
+  psa_crypto_init();
+  sli_aes_seed_mask();
+}
+
+void sl_stack_init(void)
+{
+  sl_fem_util_init();
+  sl_rail_util_pa_init();
+  sl_rail_util_pti_init();
+  sl_rail_util_rssi_init();
+  sl_bt_rtos_init();
+}
+
+void sl_internal_app_init(void)
+{
+}
