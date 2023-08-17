dac3145b2177ea8b96862a5e0e1b83db32e3e47f
Restyled.io <commits@restyled.io>
2023-08-17 05:17:52 +0000

    Restyled by clang-format

--- matter/si91x/siwx917/BRD4325B/autogen/sl_event_handler.c
+++ matter/si91x/siwx917/BRD4325B/autogen/sl_event_handler.c
@@ -1,36 +1,23 @@
 #include "sl_event_handler.h"
 
+#include "cmsis_os2.h"
+#include "rsi_board.h"
 #include "rsi_chip.h"
 #include "sli_siwx917_soc.h"
-#include "rsi_board.h"
-#include "cmsis_os2.h"
 
-void sl_platform_init(void)
-{
+void sl_platform_init(void) {
   SystemCoreClockUpdate();
   sli_si91x_platform_init();
   RSI_Board_Init();
   osKernelInitialize();
 }
 
-void sl_kernel_start(void)
-{
-  osKernelStart();
-}
+void sl_kernel_start(void) { osKernelStart(); }
 
-void sl_driver_init(void)
-{
-}
+void sl_driver_init(void) {}
 
-void sl_service_init(void)
-{
-  DEBUGINIT();
-}
+void sl_service_init(void) { DEBUGINIT(); }
 
-void sl_stack_init(void)
-{
-}
+void sl_stack_init(void) {}
 
-void sl_internal_app_init(void)
-{
-}
+void sl_internal_app_init(void) {}
