174038e038fcccae2d3759255cf849e521da18f0
Rohan Sahay <Rohan.Sahay@silabs.com>
2023-08-25 23:28:01 +0530

    Adds support of new autogenerated files (#140)

--- matter/si91x/siwx917/BRD4325x/autogen/sl_event_handler.c
+++ matter/si91x/siwx917/BRD4325x/autogen/sl_event_handler.c
@@ -3,12 +3,14 @@
 #include "cmsis_os2.h"
 #include "rsi_board.h"
 #include "rsi_chip.h"
+#include "rsi_wisemcu_hardware_setup.h"
 #include "sli_siwx917_soc.h"
 
 void sl_platform_init(void) {
   SystemCoreClockUpdate();
   sli_si91x_platform_init();
   RSI_Board_Init();
+  sl_si91x_hardware_setup();
   osKernelInitialize();
 }
 
