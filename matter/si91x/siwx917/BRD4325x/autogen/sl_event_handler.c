1361d1e6b05601f3b39b962dcf51e583727d2e5b
Chirag Bansal <chirag.bansal@silabs.com>
2023-10-11 09:59:01 +0530

    adding rtt inst rather than vcom/uart

--- matter/si91x/siwx917/BRD4325x/autogen/sl_event_handler.c
+++ matter/si91x/siwx917/BRD4325x/autogen/sl_event_handler.c
@@ -1,17 +1,18 @@
 #include "sl_event_handler.h"
 
-#include "cmsis_os2.h"
-#include "rsi_board.h"
 #include "rsi_chip.h"
 #include "rsi_nvic_priorities_config.h"
-#include "rsi_wisemcu_hardware_setup.h"
 #include "sl_device_init_nvic.h"
-#include "sl_iostream_init_instances.h"
-#include "sl_iostream_rtt.h"
-#include "sl_si91x_button_instances.h"
 #include "sli_siwx917_soc.h"
+#include "rsi_board.h"
+#include "rsi_wisemcu_hardware_setup.h"
+#include "sl_si91x_button_instances.h"
+#include "sl_iostream_rtt.h"
+#include "cmsis_os2.h"
+#include "sl_iostream_init_instances.h"
 
-void sl_platform_init(void) {
+void sl_platform_init(void)
+{
   SystemCoreClockUpdate();
   sl_si91x_device_init_nvic();
   sl_device_init_nvic();
@@ -19,24 +20,33 @@ void sl_platform_init(void) {
   RSI_Board_Init();
   sl_si91x_hardware_setup();
   osKernelInitialize();
+}
+
+void sl_kernel_start(void)
+{
+  osKernelStart();
+}
+
+void sl_driver_init(void)
+{
+  button_init_instances();
+}
+
+void sl_service_init(void)
+{
   sl_iostream_init_instances();
 }
 
-void sl_kernel_start(void) { osKernelStart(); }
+void sl_stack_init(void)
+{
+}
 
-void sl_driver_init(void) { button_init_instances(); }
+void sl_internal_app_init(void)
+{
+}
 
-void sl_service_init(void) { DEBUGINIT(); }
+void sl_iostream_init_instances(void)
+{
+  sl_iostream_rtt_init();
+}
 
-void sl_stack_init(void) {}
-
-void sl_internal_app_init(void) {}
-void sl_platform_process_action(void) {}
-
-void sl_service_process_action(void) {}
-
-void sl_stack_process_action(void) {}
-
-void sl_internal_app_process_action(void) {}
-
-void sl_iostream_init_instances(void) { sl_iostream_rtt_init(); }
--- matter/si91x/siwx917/BRD4338A/autogen/sl_event_handler.c
+++ matter/si91x/siwx917/BRD4338A/autogen/sl_event_handler.c
@@ -1,44 +1,50 @@
 #include "sl_event_handler.h"
 
-#include "cmsis_os2.h"
-#include "rsi_board.h"
 #include "rsi_chip.h"
 #include "rsi_nvic_priorities_config.h"
-#include "rsi_wisemcu_hardware_setup.h"
 #include "sl_device_init_nvic.h"
-#include "sl_iostream_init_instances.h"
-#include "sl_iostream_rtt.h"
-#include "sl_si91x_button_instances.h"
 #include "sli_siwx917_soc.h"
+#include "rsi_board.h"
+#include "rsi_wisemcu_hardware_setup.h"
+#include "sl_si91x_button_instances.h"
+#include "sl_iostream_rtt.h"
+#include "cmsis_os2.h"
+#include "sl_iostream_init_instances.h"
 
-void sl_platform_init(void) {
+void sl_platform_init(void)
+{
   SystemCoreClockUpdate();
   sl_si91x_device_init_nvic();
   sl_device_init_nvic();
   sli_si91x_platform_init();
   RSI_Board_Init();
   osKernelInitialize();
+}
+
+void sl_kernel_start(void)
+{
+  osKernelStart();
+}
+
+void sl_driver_init(void)
+{
+  button_init_instances();
+}
+
+void sl_service_init(void)
+{
   sl_iostream_init_instances();
 }
 
-void sl_kernel_start(void) { osKernelStart(); }
-
-void sl_driver_init(void) { button_init_instances(); }
-
-void sl_service_init(void) {
-  sl_sleeptimer_init();
-  DEBUGINIT();
+void sl_stack_init(void)
+{
 }
 
-void sl_stack_init(void) {}
+void sl_internal_app_init(void)
+{
+}
 
-void sl_internal_app_init(void) {}
-void sl_platform_process_action(void) {}
-
-void sl_service_process_action(void) {}
-
-void sl_stack_process_action(void) {}
-
-void sl_internal_app_process_action(void) {}
-
-void sl_iostream_init_instances(void) { sl_iostream_rtt_init(); }
+void sl_iostream_init_instances(void)
+{
+  sl_iostream_rtt_init();
+}
