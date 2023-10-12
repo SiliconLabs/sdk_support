9b719d056eb194cdbeafe2f00568263d46a0cb2b
Mathieu Kardous <mathieu.kardous@silabs.com>
2023-10-12 10:58:17 -0400

    Update BRD4319A

--- matter/efr32/mgm24/BRD4319A/config/sl_uartdrv_eusart_vcom_config.h
+++ matter/efr32/mgm24/BRD4319A/config/sl_uartdrv_eusart_vcom_config.h
@@ -65,7 +65,7 @@
 // <uartdrvFlowControlHw=> nRTS/nCTS hardware handshake
 // <uartdrvFlowControlHwUart=> UART peripheral controls nRTS/nCTS
 // <i> Default: uartdrvFlowControlHwUart
-#define SL_UARTDRV_EUSART_VCOM_FLOW_CONTROL_TYPE uartdrvFlowControlNone
+#define SL_UARTDRV_EUSART_VCOM_FLOW_CONTROL_TYPE uartdrvFlowControlHwUart
 
 // <o SL_UARTDRV_EUSART_VCOM_OVERSAMPLING> Oversampling selection
 // <eusartOVS16=> 16x oversampling
