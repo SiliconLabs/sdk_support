010621f7dfc773f2da8d8beb5ab244d6953ad61d
Rohan Sahay <rohan.sahay@silabs.com>
2023-11-07 15:33:46 +0530

    Adds updated configuration

---
+++ matter/si91x/siwx917/BRD4338A/config/sli_psa_builtin_config.h
@@ -0,0 +1,10 @@
+#ifndef SLI_PSA_BUILTIN_CONFIG_H
+#define SLI_PSA_BUILTIN_CONFIG_H
+
+// Modify this file to allow/disallow PSA crypto fallback software implementations
+
+#undef MBEDTLS_PSA_BUILTIN_ALG_ECB_NO_PADDING
+#undef MBEDTLS_PSA_BUILTIN_ALG_CMAC
+
+
+#endif // SLI_PSA_BUILTIN_CONFIG_H
