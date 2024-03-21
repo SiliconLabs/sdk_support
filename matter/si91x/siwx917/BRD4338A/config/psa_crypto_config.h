1d53880c1ede523d4676ea773720c56aeb86502c
Restyled.io <commits@restyled.io>
2024-03-21 07:21:27 +0000

    Restyled by clang-format

--- matter/si91x/siwx917/BRD4338A/config/psa_crypto_config.h
+++ matter/si91x/siwx917/BRD4338A/config/psa_crypto_config.h
@@ -18,30 +18,28 @@
 // <i> setup, until it finishes or aborts.
 // <i> When an application tries to open more keys than this value accounts for,
 // <i> the PSA API may return PSA_ERROR_INSUFFICIENT_MEMORY. Keep in mind that
-// <i> other software included in the application (e.g. wireless protocol stacks)
-// <i> also can have a need to have open keys in PSA Crypto. This could lead to
-// <i> a race condition when the application key slot count is set too low for
-// <i> the actual usage of the application, as a software stack may not fail
-// <i> gracefully in case an application opens more than its declared amount of
-// <i> keys, thereby precluding the stack from functioning.
-// <i> Default: 4
+// <i> other software included in the application (e.g. wireless protocol
+// stacks) <i> also can have a need to have open keys in PSA Crypto. This could
+// lead to <i> a race condition when the application key slot count is set too
+// low for <i> the actual usage of the application, as a software stack may not
+// fail <i> gracefully in case an application opens more than its declared
+// amount of <i> keys, thereby precluding the stack from functioning. <i>
+// Default: 4
 #define SL_PSA_KEY_USER_SLOT_COUNT (4)
 
 // <o SL_PSA_ITS_USER_MAX_FILES> PSA Maximum User Persistent Keys Count <0-1024>
 // <i> Maximum amount of keys (or other files) that can be stored persistently
 // <i> by the application through the PSA interface, when persistent storage
 // <i> support for PSA Crypto is included in the project.
-// <i> Due to caching logic, this setting does have an impact on static RAM usage.
-// <i> Note that this number is added to the potential requirements from other
-// <i> software components in the project, such that the total amount of keys
-// <i> which can be stored through the ITS backend can be higher than what is
-// <i> configured here.
-// <i>
-// <i> WARNING: When changing this setting on an application that is already
-// <i> deployed, and thus will get the change through an application upgrade,
-// <i> care should be taken to ensure that the setting is only ever increased,
-// <i> and never decreased. Decreasing this setting might cause previously
-// <i> stored keys/files to become inaccessible.
+// <i> Due to caching logic, this setting does have an impact on static RAM
+// usage. <i> Note that this number is added to the potential requirements from
+// other <i> software components in the project, such that the total amount of
+// keys <i> which can be stored through the ITS backend can be higher than what
+// is <i> configured here. <i> <i> WARNING: When changing this setting on an
+// application that is already <i> deployed, and thus will get the change
+// through an application upgrade, <i> care should be taken to ensure that the
+// setting is only ever increased, <i> and never decreased. Decreasing this
+// setting might cause previously <i> stored keys/files to become inaccessible.
 // <i>
 // <i> It is not possible to change this setting when using V3 ITS Driver.
 // <i> The file-storage indexing is dependent on the  maximum number of files,
@@ -77,13 +75,11 @@
 // <i> that those devices have full nvm3 and not enough space for the
 // <i> upgrade, (that requires an extra space to store largest key in memory
 // <i> twice), this config option can disable v3 driver and use v2 one.
-// <i> To upgrade the device, make space for the upgrade, and enable v3 driver again.
-// <i>
-// <i> WARNING: When using V3 driver, it is not possible to increase or decrease
-// <i> the value of SL_PSA_ITS_USER_MAX_FILES. If the change of
-// <i> SL_PSA_ITS_USER_MAX_FILES is required, ITS should be cleared and
-// <i> all files need to be stored again.
-// <i> Default: 1
+// <i> To upgrade the device, make space for the upgrade, and enable v3 driver
+// again. <i> <i> WARNING: When using V3 driver, it is not possible to increase
+// or decrease <i> the value of SL_PSA_ITS_USER_MAX_FILES. If the change of <i>
+// SL_PSA_ITS_USER_MAX_FILES is required, ITS should be cleared and <i> all
+// files need to be stored again. <i> Default: 1
 #define SL_PSA_ITS_SUPPORT_V3_DRIVER 1
 
 // <o SL_SE_BUILTIN_KEY_AES128_ALG_CONFIG> Built-in AES Key Mode of Operation
