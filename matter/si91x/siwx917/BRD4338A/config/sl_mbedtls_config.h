d3723512ca447c4bd6503d39d230794832d14e36
Rohan Sahay <rohan.sahay@silabs.com>
2024-04-14 22:18:55 +0530

    Add MBEDTLS_PSA_CRYPTO_C to SiWx917 autogen and config files

--- matter/si91x/siwx917/BRD4338A/config/sl_mbedtls_config.h
+++ matter/si91x/siwx917/BRD4338A/config/sl_mbedtls_config.h
@@ -107,7 +107,9 @@
 // the correct Mbed TLS / PSA Crypto options.
 #include "sli_mbedtls_config_transform_autogen.h"
 
+#ifndef SL_MBEDTLS_USE_TINYCRYPT
 // Included for backward compatibility reasons.
 #include "mbedtls/build_info.h"
+#endif // SL_MBEDTLS_USE_TINYCRYPT
 
 #endif // SL_MBEDTLS_CONFIG_H
