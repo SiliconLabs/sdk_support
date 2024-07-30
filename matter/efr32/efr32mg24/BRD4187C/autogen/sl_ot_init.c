97b9a80702d307a7ae2d320ad63dcb62c1cc3b7b
Mathieu Kardous <mathieu.kardous@silabs.com>
2024-07-30 15:54:19 -0400

    fix tab

--- matter/efr32/efr32mg24/BRD4187C/autogen/sl_ot_init.c
+++ matter/efr32/efr32mg24/BRD4187C/autogen/sl_ot_init.c
@@ -36,7 +36,7 @@
 void sl_ot_init(void)
 {
   sl_ot_create_instance();
-    sl_ot_sleep_init();  
+  sl_ot_sleep_init();  
   
   // TODO: Remove the Power Manager remove req when OT does not add an EM1 req at init
   sl_power_manager_remove_em_requirement(SL_POWER_MANAGER_EM1);  
