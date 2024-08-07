0329d50864bc24e8a37e146e20e84a2e9144bfb9
Mathieu Kardous <mathieu.kardous@silabs.com>
2024-08-07 15:24:34 -0400

    add comment

--- matter/mbedtls/tinycrypt/inc/mbedtls/build_info.h
+++ matter/mbedtls/tinycrypt/inc/mbedtls/build_info.h
@@ -2,6 +2,9 @@
 #ifndef MBEDTLS_BUILD_INFO_H
 #define MBEDTLS_BUILD_INFO_H
 
+// Custom file added to provide the necessary headers for the generated sl_mbedtls_config header.
+// File keeps the previous logic by only including check_config without needing to modfy the generated files.
+
 #include <mbedtls/check_config.h>
 
 #endif /* MBEDTLS_BUILD_INFO_H */
