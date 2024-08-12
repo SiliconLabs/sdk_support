7bf86bc0abe381ab6e7c3d5332f17e406165f687
Mathieu Kardous <mathieu.kardous@silabs.com>
2024-08-12 15:24:54 -0400

    Add include for user config

--- matter/mbedtls/tinycrypt/inc/mbedtls/build_info.h
+++ matter/mbedtls/tinycrypt/inc/mbedtls/build_info.h
@@ -6,6 +6,10 @@
 // sl_mbedtls_config header. File keeps the previous logic by only including
 // check_config without needing to modfy the generated files.
 
+#if defined(MBEDTLS_USER_CONFIG_FILE)
+#include MBEDTLS_USER_CONFIG_FILE
+#endif
+
 #include <mbedtls/check_config.h>
 
 #endif /* MBEDTLS_BUILD_INFO_H */
