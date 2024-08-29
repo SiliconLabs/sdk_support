a663ab7d01f904ea62526c5090891a09899d5e9d
bhmanda-silabs <107180296+bhmanda-silabs@users.noreply.github.com>
2024-08-29 17:59:46 +0530

    [SiWx917]  - Adding autogen and config files for brd2605a devkit board (#224)
    * Added autogen and config files for brd2605a wifi SoC board
    
    * Moved si91x support files to common folder and removed config files which are not required in CSA
    
    ---------
    
    Co-authored-by: bhmanda <bhmanda@silabs.com>

---
+++ matter/si91x/siwx917/BRD2605A/autogen/sl_event_handler.c
@@ -0,0 +1,70 @@
+#include "sl_event_handler.h"
+
+#include "system_si91x.h"
+#include "rsi_nvic_priorities_config.h"
+#include "sli_siwx917_soc.h"
+#include "rsi_board.h"
+#include "rsi_debug.h"
+#if SL_ICD_ENABLED
+#include "sl_si91x_power_manager.h"
+#include "rsi_wisemcu_hardware_setup.h"
+#include "sl_si91x_power_manager_init.h"
+#endif // SL_ICD_ENABLED
+#include "SEGGER_RTT.h"
+#include "sl_sleeptimer.h"
+#include "sl_si91x_button_instances.h"
+#include "sl_si91x_led_instances.h"
+#include "sl_ulp_timer_instances.h"
+#include "sl_iostream_rtt.h"
+#include "cmsis_os2.h"
+#include "sl_iostream_init_instances.h"
+
+void sl_platform_init(void)
+{
+  SystemCoreClockUpdate();
+  sl_si91x_device_init_nvic();
+  sli_si91x_platform_init();
+  RSI_Board_Init();
+  DEBUGINIT();
+#if SL_ICD_ENABLED
+  sl_si91x_hardware_setup();
+#endif // SL_ICD_ENABLED
+  osKernelInitialize();
+}
+
+void sl_kernel_start(void)
+{
+  osKernelStart();
+}
+
+void sl_driver_init(void)
+{
+  button_init_instances();
+  led_init_instances();
+}
+
+void sl_service_init(void)
+{
+#if SL_ICD_ENABLED
+  sl_si91x_power_manager_init();
+  sli_si91x_power_manager_configure_ram_and_peripheral();
+#endif // SL_ICD_ENABLED
+#if defined(DISPLAY_ENABLED) || defined(SL_ICD_ENABLED)
+  sl_sleeptimer_init();
+#endif // DISPLAY_ENABLED || SL_ICD_ENABLED
+  sl_iostream_init_instances();
+}
+
+void sl_stack_init(void)
+{
+}
+
+void sl_internal_app_init(void)
+{
+}
+
+void sl_iostream_init_instances(void)
+{
+  sl_iostream_rtt_init();
+}
+
